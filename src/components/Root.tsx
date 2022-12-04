061708747fb01a96f197409db652424f047a6260
Nikita Kolmogorov <backmeupplz@gmail.com>
2022-12-03 18:15:28 -0800

    add more bottom padding

--- src/components/Root.tsx
+++ src/components/Root.tsx
@@ -10,7 +10,7 @@ import ChildrenProp from 'models/ChildrenProp'
 const root = classnames(
   container('container'),
   margin('mx-auto'),
-  padding('py-4', 'px-2', 'md:px-4'),
+  padding('pt-4', 'pb-10', 'px-2', 'md:px-4'),
   maxWidth('max-w-prose')
 )
 export default function ({ children }: ChildrenProp) {
